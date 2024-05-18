9fbe8ba6cd7d60cc853147b02a49727c5d29637d
Fatvallon <43625745+Fatvallon@users.noreply.github.com>
2024-05-18 03:41:57 +0300

    Update and rename run_as_admin_exp.bat to run_as_admin_ui_2.0.bat

--- run_as_admin_ui_2.0.bat
+++ run_as_admin_ui_2.0.bat
@@ -3,4 +3,4 @@ REM Get the directory of the batch file
 set "batch_dir=%~dp0"
 
 REM Run Python script located in the same directory as the batch file
-python "%batch_dir%python-stop-service-exp.py"
+python "%batch_dir%Service stopper ui 2.0.py"
