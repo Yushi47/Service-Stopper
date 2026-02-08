2a9bbc14a6f5e402ab29e6f65db56c45b08dc283
Yushi <43625745+Yushi47@users.noreply.github.com>
2026-02-08 16:02:39 +0200

    Delete run_as_admin_ui_2.0.bat

--- run_as_admin_ui_2.0.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Get the directory of the batch file
-set "batch_dir=%~dp0"
-
-REM Run Python script located in the same directory as the batch file
-python "%batch_dir%Service stopper ui 2.0.py"
